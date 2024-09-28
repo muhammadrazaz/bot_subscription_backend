dcf198943c47821d70a4f07d6f5d5d1fa3d57a1c
Muhammad Mudassir <mudassirm734@gmail.com>
2024-09-28 18:07:32 +0500

    without  try except parse pdf

--- developer/serializers.py
+++ developer/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 
 class ProjectSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only = True)
     project_name = serializers.CharField(max_length = 50)
     total_cost = serializers.IntegerField(read_only = True)
     total_tasks = serializers.IntegerField(read_only = True)
